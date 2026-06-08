d5652f71080f28157f9d1e302c454d7622866576
ardianilyas <ardianilyas@gmail.com>
2026-06-08 17:10:53 +0700

    fix: update package name and clean up clearDb function

--- tests/helpers/clear-db.ts
+++ tests/helpers/clear-db.ts
@@ -5,14 +5,6 @@ export async function clearDb() {
   await db.execute(
     sql.raw(`
       TRUNCATE TABLE
-        goods_receipt_items,
-        goods_receipts,
-        purchase_order_items,
-        purchase_orders,
-        purchase_request_items,
-        purchase_requests,
-        products,
-        vendors
       RESTART IDENTITY
       CASCADE;
     `)
